84feb6dc680cc28576524f3332df16b75640a317
uko <iamdariusw@gmail.com>
2022-06-01 17:38:47 -0400

    added password length requirement

--- muse/validate.js
+++ muse/validate.js
@@ -3,6 +3,14 @@ console.log("JS is working.")
 const form = document.getElementById('form');
 const password = document.getElementById('password');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault
+form.addEventListener('submit', (error) => {
+    let messages = [];
+    if (password.value.length < 6)
+        messages.push('Password must be at least 6 characters');
+
+        if (messages.length > 0){
+    alert("Password must be at least 6 characters");
+    error.preventDefault();
+        }
+
 })
