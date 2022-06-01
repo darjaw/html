036dc3a4cbe3d861a4797ef03922510ecfb595d5
uko <iamdariusw@gmail.com>
2022-06-01 16:30:45 -0400

    added log-in page

---
+++ muse/validate.js
@@ -0,0 +1,8 @@
+console.log("JS is working.")
+
+const form = document.getElementById('form');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault
+})
